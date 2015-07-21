014e4ffbe7d66f4369f286cca02bd0ee041b0bf8
Steven Shang <sstevenshang@gmail.com>
2015-07-21 00:25:24 -0700

    Set up GuillotineMenu and navigation bar, some UI to be fixed

---
+++ Vac/Vac/ViewControllers/NavigationBarController.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBarController.swift
+//  Vac
+//
+//  Created by Steven Shang on 7/20/15.
+//  Copyright (c) 2015 Steven Shang. All rights reserved.
+//
+
+import UIKit
+
+class NavigationBarController: UINavigationController {
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+}
---
+++ Vac/Vac/ViewControllers/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Vac
+//
+//  Created by Steven Shang on 7/20/15.
+//  Copyright (c) 2015 Steven Shang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIButton!
+
+    @IBOutlet weak var searchBar: UITextField!
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        searchBar.attributedPlaceholder = NSAttributedString (string:"Search", attributes:[NSForegroundColorAttributeName: UIColor.grayColor()])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+
+        let destinationVC = segue.destinationViewController as! GuillotineMenuViewController
+        destinationVC.hostNavigationBarHeight = self.navigationController!.navigationBar.frame.size.height
+        
+        //destinationVC.hostTitleText = self.navigationItem.title
+        
+        destinationVC.view.backgroundColor = self.navigationController!.navigationBar.barTintColor
+        destinationVC.setMenuButtonWithImage(menuButton.imageView!.image!)
+    }
+    
+
+
+}
