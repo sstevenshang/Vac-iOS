c745e2dc86010c249f614100e226d5e4a408e83b
Steven Shang <sstevenshang@gmail.com>
2015-07-21 00:35:12 -0700

    Customized search bar

---
+++ Vac/Vac/CustomTextField.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTextField.swift
+//  Vac
+//
+//  Created by Steven Shang on 7/21/15.
+//  Copyright (c) 2015 Steven Shang. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    required init(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 15.0
+        
+    }
+    
+    
+    
+    
+}
