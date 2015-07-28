c28c639fe6ff018886f0747735e9bea15bd25600
Steven Shang <sstevenshang@gmail.com>
2015-07-28 13:19:23 -0700

    Set up word page view, about to connect to data source

---
+++ Vac/Vac/Views/WordPageView.swift
@@ -0,0 +1,25 @@
+//
+//  WordPageView.swift
+//  Vac
+//
+//  Created by Steven Shang on 7/28/15.
+//  Copyright (c) 2015 Steven Shang. All rights reserved.
+//
+
+import UIKit
+
+class WordPageView: UIScrollView {
+
+    
+    
+    
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
