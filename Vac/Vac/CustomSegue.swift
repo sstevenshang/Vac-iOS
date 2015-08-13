3848d9ea25addddc457209d72b49cd78f720362f
Steven Shang <sstevenshang@gmail.com>
2015-08-13 11:59:03 -0700

    oh my god, it's perfect, the word -> list transition is left to right now

---
+++ Vac/Vac/CustomSegue.swift
@@ -0,0 +1,27 @@
+//
+//  CustomSegue.swift
+//  Vac
+//
+//  Created by Steven Shang on 8/13/15.
+//  Copyright (c) 2015 Steven Shang. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class SegueFromLeft: UIStoryboardSegue {
+    
+    override func perform() {
+        var src: UIViewController = self.sourceViewController as! UIViewController
+        var dst: UIViewController = self.destinationViewController as! UIViewController
+        var transition: CATransition = CATransition()
+        var timeFunc : CAMediaTimingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        transition.duration = 0.25
+        transition.timingFunction = timeFunc
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromLeft
+        src.navigationController!.view.layer.addAnimation(transition, forKey: kCATransition)
+        src.navigationController!.pushViewController(dst, animated: false)
+    }
+    
+}
